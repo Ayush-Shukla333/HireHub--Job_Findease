3b6d83d4a7996b1e1f66fef9fe29a08594c6ba2a
Ayush-Shukla333 <ayushshukla1126@gmail.com>
2025-09-07 19:46:38 +0530

    changed1

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,23 +1,56 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import JobCategories from "../assets/assets.js"
 
 const JobListing = () => {
-    const {isSearched, searchFilter} = useContext(AppContext)
-    return (
-    <div>
+  const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext)
+  return (
+    <div className="container 2xl:px-20 mx-auto flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/* Sidebar */}
-      <div>
+      <div className='w-full lg:w-1/4 bg-white px-14'>
         {/* Search Filter from Hero component*/}
+        {/* Check if something is searched in the input and if something is searched then display title and location in current search sidebar 58:00*/}
+
         {
-            isSearched && (searchFilter.title !== "" || searchFilter.location !== "") && (
-                <>
-                <h3>CurrentSearch</h3>
-                <div>
+          isSearched && (searchFilter.title !== "" || searchFilter.location !== "") && (
+            <>
+              <h3 className='font-medium text-lg mb-4'>CurrentSearch</h3>
+              <div className='text-gray-600 mb-4'>
+                {searchFilter.title && (
+                  <span className="inline-flex items-center gap-2.5 bg-blue-200 border border-blue-300 text-sm px-3 py-1 rounded mb-2">
                     {searchFilter.title}
-                </div>
-                </>
-            )
+                    <XMarkIcon onClick={e => setSearchFilter(prev => ({ ...prev, title: "" }))} className="h-4 w-4 ml-2 cursor-pointer text-gray-600 hover:text-red-500"
+                    /> {/*Reset the title on clicking cross*/}
+                  </span>
+                )}
+
+                {searchFilter.location && (
+                  <span className="ml-2 inline-flex items-center gap-2.5 bg-red-200 border border-red-300 text-sm px-3 py-1 rounded mb-2">
+                    {searchFilter.location}
+                    <XMarkIcon onClick={e => setSearchFilter(prev => ({ ...prev, location: "" }))} className="h-4 w-4 ml-2 cursor-pointer text-gray-600 hover:text-red-500"
+                    /> {/*Reset the location on clicking cross*/}
+                  </span>
+                )}
+              </div>
+            </>
+          )
         }
+
+        {/* Category Filter */}
+        <div>
+          <h4>Search by Category</h4>
+          <ul>
+            {
+              JobCategories.map((category, index)=> {
+                <li key={index}>
+                  <input type="checkbox"/>
+                  <span>{category}</span>
+                </li>
+              })
+            }
+          </ul>
+        </div>
       </div>
     </div>
   )
