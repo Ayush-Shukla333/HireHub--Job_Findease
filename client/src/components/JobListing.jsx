4753444eaac17c2b8c5aa17fbe18c766ce45d163
Ayush-Shukla333 <ayushshukla1126@gmail.com>
2025-09-07 20:14:14 +0530

    Change 2

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import JobCategories from "../assets/assets.js"
 
 const JobListing = () => {
   const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext)
   return (
-    <div className="container 2xl:px-20 mx-auto flex-col lg:flex-row max-lg:space-y-8 py-8">
+    <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/* Sidebar */}
       <div className='w-full lg:w-1/4 bg-white px-14'>
         {/* Search Filter from Hero component*/}
@@ -38,15 +37,17 @@ const JobListing = () => {
         }
 
         {/* Category Filter */}
-        <div>
-          <h4>Search by Category</h4>
+        <div className="max-lg:hidden">
+          <h4 className="font-medium text-lg py-4 mb-2">Search by Category</h4>
           <ul>
             {
               JobCategories.map((category, index)=> {
-                <li key={index}>
-                  <input type="checkbox"/>
-                  <span>{category}</span>
+                return (
+                <li key={index} className="flex items-center gap-2 my-1">
+                  <input  className="scale-125" type="checkbox"/>
+                  {category}
                 </li>
+                )
               })
             }
           </ul>
